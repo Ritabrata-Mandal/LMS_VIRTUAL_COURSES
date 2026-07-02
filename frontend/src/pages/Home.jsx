f6f59214e75d0caccd8426ac335aaad03e5611b0
Ritabrata Mandal <ritabratam15042005@gmail.com>
2026-07-03 01:15:58 +0530

    Initial commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -26,14 +26,14 @@ const Home = () => {
         <span className='lg:text-[70px] absolute md:text-[40px] lg:top-[10%] top-[15%] w-[100%] flex items-center justify-center text-white font-bold text-[20px]'>Grow Your Skills to Advance</span>
         <span className='lg:text-[70px] absolute md:text-[40px] lg:top-[18%] top-[20%] w-[100%] flex items-center justify-center text-white font-bold text-[20px]'>Your Career path</span>
         
-        <div className='absolute lg:top-[30%] top-[75%] md:top-[80%] w-[100%] flex justify-center items-center gap-3 flex-wrap'>
+        {/* <div className='absolute lg:top-[30%] top-[75%] md:top-[80%] w-[100%] flex justify-center items-center gap-3 flex-wrap'>
           <button className='px-[20px] py-[10px] border-2 lg:border-white border-black lg:text-white text-black rounded-[10px] txet-[18px] font-light flex gap-2 cursor-pointer' onClick={()=>navigate('/allcourses')}><p className='mt-[2px]'>View All Courses</p> <SiViaplay className='w-[30px] h-[30px] lg:fill-white fill-black'/>
           </button>
 
           <button className='px-[20px] py-[10px] lg:bg-white bg-black lg:text-black text-white rounded-[10px] txet-[18px] font-light flex gap-2 cursor-pointer' onClick={()=>navigate('/search')}><p className='mt-1'>Search with AI</p> <img src={ai} alt=""  className='h-[30px] h-[30px] rounded-full rounded-full hidden lg:block'/>
           <img src={ai1} alt=""  className='h-[35px] h-[35px] rounded-full rounded-full lg:hidden'/>
           </button>
-        </div>
+        </div> */}
       </div>
       <Logos/>
       <ExploreCourses/>
